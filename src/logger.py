fd8ee6b2bcfd3576e345fa736664235e0c951804
asarium <asarium@gmail.com>
2018-05-20 18:50:11 +0200

    Fix Travis CI (again)

--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,11 @@
 # This exposes a function which writes to the GDB log if available and to stdout otherwise
 try:
+    from gdb import write
     import gdb
 
 
     def log_message(msg: str):
-        gdb.write(str(msg) + "\n", gdb.STDLOG)
+        write(str(msg) + "\n", gdb.STDLOG)
 except ImportError:
     def log_message(msg: str):
         print(msg)
